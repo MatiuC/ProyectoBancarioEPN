35224aa60d6945b851d096d0ddb8495ad5ce6976
BryanA <bryan.ayala02@epn.edu.ec>
2025-02-10 00:35:45 -0500

    Empezando a modificar RegistroClientesForm

--- BancoEPN/src/App.java
+++ BancoEPN/src/App.java
@@ -1,11 +1,23 @@
-import UserInterface.Form.LoginPanel;
+import javax.swing.*;
+import com.formdev.flatlaf.FlatLightLaf;
+import UserInterface.Form.RegistroClientesForm;
 
 public class App {
     public static void main(String[] args) {
         try {
-            // Crear y mostrar la ventana de login
-            LoginPanel loginFrame = new LoginPanel();
-            loginFrame.setVisible(true);
+            // Configurar el Look and Feel
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            SwingUtilities.invokeLater(() -> {
+                try {
+                    // Crear y mostrar la ventana de registro de clientes
+                    RegistroClientesForm registroFrame = new RegistroClientesForm();
+                    registroFrame.getContentPane().add(registroFrame.getFormularioPanel());
+                    registroFrame.setVisible(true);
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Error al iniciar la aplicación: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
+            });
         } catch (Exception e) {
             System.out.println("Error al iniciar la aplicación: " + e.getMessage());
             e.printStackTrace();
